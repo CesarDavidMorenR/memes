a0d37daeee0f6e04f2c3f7157c743121c67400c4
Cesar D. Moreno <cesarmoreno@infofuturo.com>
2023-01-29 16:41:27 +0100

    Changes to common layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,7 +22,7 @@
         },
       }
   </script>
-  <title>LaraGigs | Find Laravel Jobs & Projects</title>
+  <title>Memes WebSite</title>
 </head>
 
 <body class="mb-48">
@@ -36,7 +36,7 @@
         </span>
       </li>
       <li>
-        <a href="/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-gear"></i> Manage Listings</a>
+        <a href="/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-gear"></i> Manage Memes</a>
       </li>
       <li>
         <form class="inline" method="POST" action="/logout">
@@ -62,9 +62,9 @@
   </main>
   <footer
     class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
-    <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
+    <p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>
 
-    <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a>
+    <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post a Meme😝</a>
   </footer>
 
   <x-flash-message />
